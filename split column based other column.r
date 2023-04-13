9d03c46d2433e7f1634843c822a3cceee1c3e25e
rezgardarvand <rezgardarvand@yahoo.com>
2023-04-13 17:14:42 +0430

    last file

---
+++ split column based other column.r
@@ -0,0 +1,21 @@
+#split column based other column.r
+data<-data.frame(x=c("a","b","c","d","e", "s"), 
+                 y=c("b","c","f", "g","a",""),
+                 point=(c(20,23,63,32,25,20)))
+library(data.table)
+DT <- fread("plot1   data    plot2   data2 plot3 data3
+            a       25       c       28     k     6
+            b       46       e       18  NA  NA
+            c       21       j       66  NA  NA
+            d       32  NA  NA  NA  NA
+            e       1   NA  NA  NA  NA")
+L <- lapply( split.default(DT, f = 2:(ncol(DT)+1) %/% 2 ),
+             function(x) setnames(x, old = grep("^plot", names(x), value = TRUE), new = "plot") )
+# merge function
+mymerge <- function(x, y) merge.data.table(x, y, all = TRUE, by = "plot", allow.cartesian = TRUE)
+# full merge all the data.table in the list L using Reduce()
+ans <- Reduce(mymerge, L)[!is.na(plot), ]
+# replace NA with 0 (zero)
+ans[is.na(ans)] <- 0
+
+
